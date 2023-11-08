b0a121802b560ce7e7dce7691ebc70ad93144f58
maksims.bezenars <maksims.bezenar@mapon.com>
2023-11-08 22:07:39 +0200

    set sb default bg black

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,20 @@
-import type { Preview } from "@storybook/react";
-import '!style-loader!css-loader!sass-loader!../src/assets/styles/main.scss'
+import type { Preview } from '@storybook/react';
+import '!style-loader!css-loader!sass-loader!../src/assets/styles/main.scss';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        backgrounds: {
+            default: 'dark',
+            values: [{ name: 'dark', value: '#000000' }],
+        },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
