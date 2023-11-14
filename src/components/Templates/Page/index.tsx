fcba2f8feb9c749ce96100b13caf1ffdf5a9016a
maksims.bezenars <maksims.bezenar@mapon.com>
2023-11-14 23:50:31 +0200

    Create page tmp

--- src/components/Templates/Page/index.tsx
+++ src/components/Templates/Page/index.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
-import s from './Page.module.scss';
-import cn from '@helpers/cn';
 import logo from '@img/logo.png';
+import s from './Page.module.scss';
 import Pagination from '@molecules/Pagination';
 import Card from '@atoms/Card';
 import Image from '@atoms/Image';
 import Nav from '@molecules/Nav';
+import usePagination from '@hooks/usePagination';
+import cn from '@helpers/cn';
 
 const Page: React.FC = () => {
+    const {handleChangePage, ...restPaginationState} = usePagination({active: 1, totalPages: 10});
+
     return (
         <div className={cn('bg--black py-5 flex jc-center', s.appWrapper)}>
             <div className={s.appContainer}>
@@ -23,7 +26,7 @@ const Page: React.FC = () => {
                 </main>
                 <footer>
                     <Card className="flex jc-center ai-center">
-                        <Pagination active={1} totalPages={10} onChange={() => {}} />
+                        <Pagination {...restPaginationState} onChange={handleChangePage} />
                     </Card>
                 </footer>
             </div>
