abafd68666c533cd7d2826f2c449fa61821ad1e0
maksims.bezenars <maksims.bezenar@mapon.com>
2023-11-17 00:00:34 +0200

    add pagination slice

--- src/store/slices/RickAndMortyApi/index.ts
+++ src/store/slices/RickAndMortyApi/index.ts
@@ -1,8 +1,9 @@
 import parseObjectToParamsString from '@helpers/parseObjectToParamsString';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { API_URL } from '../constants';
+import { API_URL } from '../../../constants';
 import type { N_ReqParams } from '@t/requestParams';
 import type { N_Response } from '@t/responses';
+import { resetPaginationState } from '../pagination';
 
 export const rickAndMortyApi = createApi({
     reducerPath: 'api',
@@ -10,6 +11,13 @@ export const rickAndMortyApi = createApi({
     endpoints: (builder) => ({
         getCharacters: builder.query<N_Response.T_CharactersRes, N_ReqParams.I_Characters>({
             query: (params = {}) => `character${parseObjectToParamsString(params)}`,
+            onQueryStarted: async (_, {dispatch, queryFulfilled}) => {
+                const paginationRes = await queryFulfilled;
+                console.log('123');
+                await dispatch(resetPaginationState({
+                    totalPages: paginationRes.data.info.pages
+                }));
+            }
         }),
         getEpisodes: builder.query<N_Response.T_EpisodesRes, N_ReqParams.I_Episodes>({
             query: (params = {}) => `episode${parseObjectToParamsString(params)}`,
---
+++ src/store/slices/pagination/initialState.ts
@@ -0,0 +1,6 @@
+import type { I_PaginationState } from '@t/index';
+
+export default {
+    active: 1,
+    totalPages: 0,
+} as I_PaginationState;
