a65cdde1724421fd7e9ae75282899ceee58d6b7c
maksims.bezenars <maksims.bezenar@mapon.com>
2023-11-17 11:47:11 +0200

    rmv console in api slice

--- src/store/slices/RickAndMortyApi/index.ts
+++ src/store/slices/RickAndMortyApi/index.ts
@@ -11,13 +11,15 @@ export const rickAndMortyApi = createApi({
     endpoints: (builder) => ({
         getCharacters: builder.query<N_Response.T_CharactersRes, N_ReqParams.I_Characters>({
             query: (params = {}) => `character${parseObjectToParamsString(params)}`,
-            onQueryStarted: async (_, {dispatch, queryFulfilled}) => {
+            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
                 const paginationRes = await queryFulfilled;
-                console.log('123');
-                await dispatch(resetPaginationState({
-                    totalPages: paginationRes.data.info.pages
-                }));
-            }
+
+                dispatch(
+                    resetPaginationState({
+                        totalPages: paginationRes.data.info.pages,
+                    })
+                );
+            },
         }),
         getEpisodes: builder.query<N_Response.T_EpisodesRes, N_ReqParams.I_Episodes>({
             query: (params = {}) => `episode${parseObjectToParamsString(params)}`,
